35b59c7c641de6ec0f6357ce93c6e637533c0164
Eduardo Duraes <eduvogtduraes@gmail.com>
2026-04-08 01:07:08 -0300

    add: app to find hsv mask points

---
+++ apps/constants/hsv_contants.py
@@ -0,0 +1,12 @@
+# path to image to be analyzed 
+FILE_PATH = '/home/duraes/pucrs/5th_semester/comp_vision/trafficLightsDetection/S2TLD（720x1280）/normal_1/JPEGImages/000013.jpg'
+
+WINDOW_TRACKBAR_NAME = 'HSV Trackbars'
+WINDOW_ORIGINAL_IMAGE_NAME = 'Input Image'
+WINDOW_MASK_IMAGE_NAME = 'Masked Input Image'
+LOWER_HUE_NAME = 'Lower Hue'
+LOWER_SATURATION_NAME = 'Lower Saturation'
+LOWER_VALUE_NAME = 'Lower Value'
+UPPER_HUE_NAME = 'Upper Hue'
+UPPER_SATURATION_NAME = 'Upper Saturation'
+UPPER_VALUE_NAME = 'Upper Value'
