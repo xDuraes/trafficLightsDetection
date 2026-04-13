1b6c219685a2246a80b36bcc1751ac58786466bc
Eduardo Duraes <eduvogtduraes@gmail.com>
2026-04-13 00:04:36 -0300

    fix: parser

--- src/utils/arguments.py
+++ src/utils/arguments.py
@@ -2,8 +2,9 @@ from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser()
-    parser.parse_args(
+    parser.add_argument(
         "--input_dir",
         type=str,
         help="input the path to the dir containing the images"
     )
+    return parser.parse_args()
