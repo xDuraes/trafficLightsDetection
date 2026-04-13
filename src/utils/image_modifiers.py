3656e76fe338e094c47215bb862c0ea12b661a51
Eduardo Duraes <eduvogtduraes@gmail.com>
2026-04-13 00:43:03 -0300

    up: logic on masking function modified

--- src/utils/image_modifiers.py
+++ src/utils/image_modifiers.py
@@ -1,19 +1,19 @@
 import cv2 as cv
 from cv2.typing import MatLike
 
-def applyMask(image:MatLike, boundaries:list) -> MatLike:
+def applyMask(image:MatLike, boundaries:tuple) -> MatLike:
     """
     Function to apply a mask to isolate colors in an image
 
     :param image: OpenCV processed image
     :type image: MatLike
-    :param boundaries: List of the n boundaries to be applied in the image sturctured as follows: 
-    [(lower boundary, upper boundary)]
-    :type boundaries: list
-    :return: Description
+    :param boundaries: tuple of two elements containing the lower and upper boundary, respectively
+    :type boundaries: tuple
+    :return: Returns an binarized masked version of the original image
     :rtype: MatLike
     """
-    img = image
-    for lower_boundary, upper_boundary in boundaries:
-        img = cv.inRange(img, lower_boundary, upper_boundary)
-    return img
+    lower_boundary, upper_boundary = boundaries
+
+    masked_image = cv.inRange(image, lower_boundary, upper_boundary)
+
+    return masked_image
