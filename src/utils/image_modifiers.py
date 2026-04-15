456d9275dbb1660e1830169d728050ee16c3fa9f
Eduardo Duraes <eduvogtduraes@gmail.com>
2026-04-14 22:55:58 -0300

    add: functions to find and draw circles

--- src/utils/image_modifiers.py
+++ src/utils/image_modifiers.py
@@ -1,5 +1,9 @@
-from cv2 import inRange, findContours, drawContours, RETR_TREE, CHAIN_APPROX_SIMPLE
+from cv2 import (
+    inRange, findContours, drawContours, HoughCircles, circle,
+    RETR_TREE, CHAIN_APPROX_SIMPLE, HOUGH_GRADIENT
+    )
 from cv2.typing import MatLike
+import numpy as np
 
 def applyMask(image:MatLike, boundaries:tuple) -> MatLike:
     """
@@ -39,3 +43,49 @@ def findAndDrawContours(image:MatLike, masked_image:MatLike, color:tuple):
     img_contour = drawContours(image, contour, -1, color, 3)
 
     return img_contour 
+
+def findCircles(
+        image:MatLike,
+        dp:float,
+        minDist:float,
+        param1:float,
+        param2:float,
+        minRadius:int,
+        maxRadius:int,
+):
+    """
+    
+    """  
+    
+    # circle detection
+    circles = HoughCircles(
+        image=image,
+        method=HOUGH_GRADIENT,
+        dp=dp,
+        minDist=minDist,
+        param1=param1,
+        param2=param2,
+        minRadius=minRadius,
+        maxRadius=maxRadius
+    )
+
+    return circles
+
+def drawCircles(image:MatLike, circles:MatLike, color:tuple, drawCenter:bool):
+    
+    if color not in [(0, 255, 0), (0, 255, 255), (0, 0, 255)]:
+        raise ValueError(f"Color {color} does not exist in a traffic light")
+
+    # circle drawing in image
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            if drawCenter:
+                circle(image, center, 1, color, 3)
+            # circle radius
+            radius = i[2]
+            circle(image, center, radius, color, 3)
+
+    return image
